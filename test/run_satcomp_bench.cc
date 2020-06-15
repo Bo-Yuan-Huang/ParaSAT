1469f23f9f03a4a168961ce190346d138666f968
Bo-Yuan Huang <byhuang1992@gmail.com>
2020-06-14 23:14:02 -0400

    Setup Google test

---
+++ test/run_satcomp_bench.cc
@@ -0,0 +1,14 @@
+#include <parasat/simp/SimpSolver.h>
+
+#include "gtest/gtest.h"
+
+#include "config.h"
+
+namespace parasat {
+
+TEST(TestSatComp, Random) {
+  SimpSolver s;
+  //
+}
+
+}; // namespace parasat
