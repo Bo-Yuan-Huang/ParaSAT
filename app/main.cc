6644496258ca0a26d45ec86c51266e83d10faf9e
Bo-Yuan Huang <byhuang1992@gmail.com>
2020-06-14 23:49:03 -0400

    Ensure app is linked to ext libs

--- app/main.cc
+++ app/main.cc
@@ -1,9 +1,37 @@
+// =============================================================================
+// MIT License
+//
+// Copyright (c) 2020 Princeton University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// =============================================================================
+
+#include <fmt/format.h>
+#include <lyra/lyra.hpp>
+
+#include <parasat/core/Solver.h>
 #include <parasat/simp/SimpSolver.h>
 
-int main() {
+int main(int argc, char** argv) {
 
-  parasat::SimpSolver s;
+  parasat::SimpSolver simp;
+  parasat::Solver s;
 
-  //
   return 0;
 }
